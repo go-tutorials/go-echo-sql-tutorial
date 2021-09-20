1dda97c4d172d0e62341639668da5e15fa29e778
MinhTran1997 <52104501+MinhTran1997@users.noreply.github.com>
2021-09-20 19:44:17 +0700

    Update root.go

--- internal/app/root.go
+++ internal/app/root.go
@@ -2,14 +2,15 @@ package app
 
 import (
 	"github.com/core-go/log"
-	mid "github.com/core-go/log/middleware"
 	sv "github.com/core-go/service"
 	"github.com/core-go/sql"
+
+	l "go-service/pkg/log"
 )
 
 type Root struct {
 	Server     sv.ServerConfig `mapstructure:"server"`
 	Sql        sql.Config      `mapstructure:"sql"`
 	Log        log.Config      `mapstructure:"log"`
-	MiddleWare mid.LogConfig   `mapstructure:"middleware"`
+	MiddleWare l.LogConfig     `mapstructure:"middleware"`
 }
